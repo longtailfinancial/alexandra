010b213a3e6a6fd9ce485b700d34cf361fa907f6
JohannSuarez <johann.suarez92@gmail.com>
2022-02-17 15:03:07 -0800

    Tabula rasa

--- src/commands/alexandra.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'alex',
-	description: 'A simple query',
-	execute(message, args) {
-		message.channel.send(`So be it, let your will be done.`);
-	},
-};
--- utilities/run.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-# This script is meant for the SystemD Unit
-# cd [ABSOLUTE_DIRECTORY_OF_PROJECT_ROOT]
-cd /root/alexandra
-echo "Calling bot.js"
-node src/bot.js &
