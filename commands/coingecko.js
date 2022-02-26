d5d557d08e86a4e7a07f5efb7fefba98a19913f9
JohannSuarez <johann.suarez92@gmail.com>
2022-02-26 14:20:00 -0800

    Porting over boatswan progress ( Crypto price checker cmd )

---
+++ commands/coingecko.js
@@ -0,0 +1,83 @@
+'use strict';
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios');
+
+// Grabbing JSON to check. 
+// It has ticker symbols of all Coingecko listed tokens.
+const fs = require('fs');
+let rawdata = fs.readFileSync('data/crypto_data.json')
+let token_info = JSON.parse(rawdata);
+
+// Provided symbol will be given by discord user
+let currency = 'usd'
+
+function get_token_id(ticker) {
+
+  let token_id = ticker || "btc";
+
+  try {
+    token_id = token_info[token_id]['id'];
+  } catch (error) {
+    console.log(error);
+    console.log("Ticker not found. Aborting...")
+    return 0;
+  }
+  return token_id;
+}
+
+async function get_token_price(ticker) {
+
+  let token_id = get_token_id(ticker);
+  let http_string = `https://api.coingecko.com/api/v3/simple/price?ids=${token_id}&vs_currencies=${currency}`;
+
+  let token_data = "str";
+  await axios.get(http_string)
+    .then(res => {
+      let headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+      //console.log('Status Code:', res.status);
+      //console.log('Date in Response header:', headerDate);
+
+      token_data = res.data;
+
+    }).catch(err => {
+      console.log('Error: ', err.message);
+    });
+
+  return token_data;
+}
+
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('price')
+    .setDescription('Check crypto price by ticker symbol.'),
+  execute(message, args) {
+
+    if (args.length != 1) {
+      message.reply("Format: !price [Token ticker symbol]");
+      return 0;
+    }
+
+    let input_string = args[0].toLowerCase();
+
+    get_token_price(input_string).then((res) => {
+
+      if (Object.keys(res).length === 0) {
+        let reply = "Could not retrieve token data."
+        console.log(reply);
+        message.reply(reply);
+        return 0;
+      }
+
+      //let parsed = JSON.stringify(res);
+      let token_name = Object.keys(res)[0];
+
+      // Grabbing the value of the value and turning the result into a string. 
+      let token_price = Object.values(Object.values(res)[0]).toString();
+      let token_info = `Current price of ${token_name} token is: $${token_price} USD.`;
+
+      message.reply(token_info);
+
+    })
+  }
+};
