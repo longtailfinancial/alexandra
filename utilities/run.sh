13ceefdbb5cb4d80e5b3f9c67b7fb5c9a8b24c0e
JohannSuarez <johann.suarez92@gmail.com>
2022-02-27 03:05:22 -0800

    Minor tweaks to run.sh and alexandra.service

--- utilities/run.sh
+++ utilities/run.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/bash
 # This script is meant to be used by the SystemD Unit
 
-# cd [ABSOLUTE_DIRECTORY_OF_PROJECT_ROOT]
-echo "Calling bot.js"
-node src/bot.js &
-
+cd [ABSOLUTE_DIRECTORY_OF_PROJECT_ROOT]
+echo "Calling index.js"
+node index.js &
