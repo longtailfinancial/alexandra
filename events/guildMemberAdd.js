823e805d5c33f4ddc24a22021a52d79da1ab667d
JohannSuarez <johann.suarez92@gmail.com>
2022-02-26 22:55:54 -0800

    Edited events/guildMemberAdd.js to source from config.json

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,8 +1,10 @@
+const { channelGreetId, channelGreetText } = require('../config.json');
+
 module.exports = {
 	name: 'guildMemberAdd',
 	once: false,
 	async execute(member) {
 		console.log("Someone Joined");
-		member.guild.channels.cache.get('841431725603028993').send(`${member} Welcome to Costco. I love you.`); 
+		member.guild.channels.cache.get(channelGreetId).send(`${member} ${channelGreetText}`); 
 	},
 };
